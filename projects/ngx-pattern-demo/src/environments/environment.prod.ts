a71c3acb2dc7cd8bfd59f95a05fbc9b2f7c14076
Angel Vladov <angel@avladov.com>
2022-11-29 13:14:22 +0200

    correct package json import

--- projects/ngx-pattern-demo/src/environments/environment.prod.ts
+++ projects/ngx-pattern-demo/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
-import { version } from '../../../ngx-pattern/package.json';
+import packageJson from '../../../ngx-pattern/package.json';
 
 export const environment = {
   production: true,
-  version,
+  version: packageJson.version as string,
 };
